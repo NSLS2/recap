f7ab65b221c42e31fbbfc43d492e0d20a2029aba
Shekar V <vshekar1@bnl.gov>
2025-09-06 21:51:03 -0400

    Added client and DSL related classes

---
+++ recap/client/base_client.py
@@ -0,0 +1,40 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from contextlib import contextmanager
+from typing import Optional
+
+from recap.dsl.process_builder import ProcessTemplateBuilder, ProcessRunBuilder
+from recap.dsl.resource_builder import ResourceTemplateBuilder
+
+class RecapClient:
+    def __init__(self, url: Optional[str]=None, echo: bool = False, session= None):
+        if url is not None:
+            self.engine = create_engine(url, echo=echo)
+            self.Session = sessionmaker(bind=self.engine, expire_on_commit=False, future=True)
+        if session is not None:
+            self._session = session
+    
+    @contextmanager
+    def session(self):
+        """Yield a Session with transaction boundaries."""
+        with self.Session() as session:
+            try:
+                with session.begin():
+                    yield session
+            finally:
+                # Session closed by context exit
+                ...
+
+    def process_template(self, name: str, version: str) -> ProcessTemplateBuilder:
+        session = self._session
+        return ProcessTemplateBuilder(session=session, name=name, version=version)
+
+    def process_run(self, name: str, template_name: str, version: str):
+        return ProcessRunBuilder(session=self._session, name=name,
+                                 template_name=template_name, version=version)
+
+    def resource_template(self, name: str, type_names: list[str]):
+        return ResourceTemplateBuilder(session=self._session, name=name,
+                                       type_names=type_names)
+
+        
--- recap/utils/general.py
+++ recap/utils/general.py
@@ -1,3 +1,5 @@
+from slugify import slugify
+
 def generate_uppercase_alphabets(n: int) -> list:
     if n < 1:
         raise ValueError("The number must be a positive integer.")
@@ -12,3 +14,19 @@ def generate_uppercase_alphabets(n: int) -> list:
         return "".join(reversed(result))
 
     return [get_letter(i) for i in range(1, n + 1)]
+
+def make_slug(value: str) -> str:
+    """
+    Generate a slug that is always a valid Python identifier.
+    """
+    regex_pattern = r'[^a-z0-9_]+'  # allow only lowercase letters, digits, underscores
+    slug = slugify(
+        value,
+        lowercase=True,
+        separator="_",
+        regex_pattern=regex_pattern,
+    )
+    # Ensure it doesn't start with a digit (prepend underscore if so)
+    if slug and slug[0].isdigit():
+        slug = f"_{slug}"
+    return slug
